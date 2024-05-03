971fe63c9e367dbe05a0b1981d34d45ffef9405a
m1kc (Max Musatov) <m1kc@yandex.ru>
2024-05-04 02:22:49 +0400

    Add one more example

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,8 @@ fn main() {
 	// vm.load("123.");
 	// vm.load("99b:\n[b;0=[\"No more bottles of beer\"]?b;1=[\"1 more bottle of beer\"]?b;1>[b;.\" bottles of beer\"]?]a:\n[b;0>][a;!\" on the wall\"10,a;!10,\"Take one down, pass it around\"10,b;1-b:a;!\" on the wall\n\"]#");
 	// vm.load("^^^,,,");
-	vm.load("[$ 1 > [1- $ f;! \\ 1- f;! +]?]f:       33 f;! . {compute & print 33th fibonacci number}");
+	// vm.load("[$ 1 > [1- $ f;! \\ 1- f;! +]?]f:       33 f;! . {compute & print 33th fibonacci number}");
+	vm.load("1999 9[1-$][\\$@$@$@$@\\/*=[1-$$[%\\1-$@]?0=[\\$.' ,\\]?]?]#");
 	println!("ok, {:?}", start.elapsed());
 
 	println!("Running...");
