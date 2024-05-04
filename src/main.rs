16acb7971fba472695841092b1876522cb05a566
m1kc (Max Musatov) <m1kc@yandex.ru>
2024-05-04 15:41:09 +0400

    Implement running code from files

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
 mod falselang;
 use falselang::vm::FalseVM;
-use std::time::Instant;
+use std::{io::Read, time::Instant};
 
 
 fn main() {
+	let args: Vec<String> = std::env::args().collect();
+	// if a filename is provided, read from file
+	let code = if args.len() == 2 {
+		std::fs::read_to_string(&args[1]).expect("Failed to read file")
+	} else {
+		// otherwise, read from stdin until EOF
+		let mut code = String::new();
+		std::io::stdin().read_to_string(&mut code).expect("Failed to read stdin");
+		code
+	};
+
 	let mut vm = FalseVM::new();
 	print!("Parsing... ");
 	let start = Instant::now();
-	// vm.load("   2 2 +  ");
-	// vm.load("[2 2+]![2 2+]!");
-	// vm.load("1[\"hello\"]? 0[\"hello\"]?");
-	// vm.load("[$1=$[\\%1\\]?~[$1-f;!*]?]f:    \"factorial of \" 5 $. \" is \" f;!.");  // factorial example
-	// vm.load("123.");
-	// vm.load("99b:\n[b;0=[\"No more bottles of beer\"]?b;1=[\"1 more bottle of beer\"]?b;1>[b;.\" bottles of beer\"]?]a:\n[b;0>][a;!\" on the wall\"10,a;!10,\"Take one down, pass it around\"10,b;1-b:a;!\" on the wall\n\"]#");
-	// vm.load("^^^,,,");
-	vm.load("[$ 1 > [1- $ f;! \\ 1- f;! +]?]f:       33 f;! . {compute & print 33th fibonacci number}");
-	// vm.load("1999 9[1-$][\\$@$@$@$@\\/*=[1-$$[%\\1-$@]?0=[\\$.' ,\\]?]?]#");
-	// vm.load("");
+	vm.load(&code);
 	println!("ok, {:?}", start.elapsed());
 
-	println!("Running...");
+	println!("Running...\n");
 	let start = Instant::now();
 	// vm.verbose = true;
 	vm.run();
-	println!("\nRun complete, {:?}", start.elapsed());
+	println!("\n\nRun complete, {:?}", start.elapsed());
 }
